5a8f33c3ee8a874a09e0007653748715557562ee
ozzy-cydeo <ozzy@cybertekschool.com>
2022-06-21 20:18:08 -0400

    Security

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -93,6 +93,7 @@ public class UserServiceImpl implements UserService {
             user.setIsDeleted(true);
             user.setUserName(user.getUserName() + "-" + user.getId());
             userRepository.save(user);
+            keycloakService.delete(username);
         }
 
     }
