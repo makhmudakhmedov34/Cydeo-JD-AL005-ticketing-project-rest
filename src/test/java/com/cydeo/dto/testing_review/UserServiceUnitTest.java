40bce73412be998be67dca6282169b443ad88618
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2024-01-14 13:16:04 -0500

    ProjectControllerTest added

--- src/test/java/com/cydeo/dto/testing_review/UserServiceUnitTest.java
+++ src/test/java/com/cydeo/dto/testing_review/UserServiceUnitTest.java
@@ -1,4 +1,120 @@
 package com.cydeo.dto.testing_review;
 
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.KeycloakService;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceUnitTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private ProjectService projectService;
+    @Mock
+    private TaskService taskService;
+    @Mock
+    private KeycloakService keycloakService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private UserServiceImpl userService;
+    @Spy
+    private UserMapper userMapper = new UserMapper(new ModelMapper());
+
+    User user;
+    UserDTO userDTO;
+
+    @BeforeEach
+    void setUp(){
+        user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setUserName("user");
+        user.setPassWord("Abc1");
+        user.setEnabled(true);
+        user.setRole(new Role("Manager"));
+
+        userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setFirstName("John");
+        userDTO.setLastName("Doe");
+        userDTO.setUserName("user");
+        userDTO.setPassWord("Abc1");
+        userDTO.setEnabled(true);
+
+        RoleDTO roleDTO = new RoleDTO();
+        roleDTO.setDescription("Manager");
+
+        userDTO.setRole(roleDTO);
+    }
+
+    private List<User> getUsers() {
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setFirstName("Emily");
+
+        return List.of(user,user2);
+    }
+
+    private List<UserDTO> getUsersDTOs() {
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setId(2L);
+        userDTO2.setFirstName("Emily");
+
+        return List.of(userDTO,userDTO2);
+    }
+
+    @Test
+    void should_list_all_users(){
+
+        when(userRepository.findAllByIsDeletedOrderByFirstNameDesc(false)).thenReturn(getUsers());
+
+        List<UserDTO> expectedUserDTOs = getUsersDTOs();
+        List<UserDTO> actualUserDTOs = userService.listAllUsers();
+
+        //assertEquals(expectedUserDTOs,actualUserDTOs);
+
+        //AssertJ
+        assertThat(actualUserDTOs).usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedUserDTOs);
+    }
+
+    @Test
+    void should_find_user_by_username(){
+        when(userRepository.findByUserNameAndIsDeleted(anyString(),anyBoolean())).thenReturn(user);
+        //lenient().when(userRepository.findByUserNameAndIsDeleted(anyString(),anyBoolean())).thenReturn(user);
+
+        UserDTO actualUserDTO = userService.findByUserName("user");
+        UserDTO expectedUserDTO = userDTO;
+
+        assertThat(actualUserDTO).usingRecursiveComparison().isEqualTo(expectedUserDTO);
+
+    }
+
 }
